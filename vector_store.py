bb7f456d280436dad8ef73672c609fac4eee5e35
kamaliravi57 <0910thi@gmail.com>
2026-06-17 22:52:12 +0530

    Fix embeddings shape issue

--- vector_store.py
+++ vector_store.py
@@ -1,11 +1,20 @@
 import faiss
+import numpy as np
 from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def create_vector_store(chunks):
 
+    if len(chunks) == 0:
+        raise ValueError("No chunks generated. PDF text is empty.")
+
     embeddings = model.encode(chunks)
+
+    embeddings = np.array(
+        embeddings,
+        dtype="float32"
+    )
 
     dimension = embeddings.shape[1]
 
@@ -20,7 +29,15 @@ def retrieve_chunk(question, index, chunks):
 
     question_embedding = model.encode([question])
 
-    distances, indices = index.search(question_embedding, 3)
+    question_embedding = np.array(
+        question_embedding,
+        dtype="float32"
+    )
+
+    distances, indices = index.search(
+        question_embedding,
+        3
+    )
 
     context = "\n\n".join(
         [chunks[i] for i in indices[0]]
